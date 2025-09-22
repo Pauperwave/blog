fcad6eeb83291f18096b57caae12d9e981cd3aaf
Emanuele Nardi <emanuelenardi.dev@gmail.com>
2025-09-23 01:08:59 +0200

    refactor content configuration to define blog collection with custom schema

--- content.config.ts
+++ content.config.ts
@@ -1,11 +1,28 @@
-
-import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { defineCollection, defineContentConfig, property } from '@nuxt/content'
+import { z } from 'zod/v4'
 
 export default defineContentConfig({
   collections: {
-    content: defineCollection({
+    blog: defineCollection({
+      // Specify the type of content in this collection
       type: 'page',
-      source: '**/*.md'
+      // Define the path to the markdown files for this collection
+      // includes all Markdown files within the content/blog and its subdirectories.
+      source: 'blog/**/*.md',
+      // Define custom schema for docs collection
+      schema: z.object({
+        title: z.string(),
+        description: z.string().optional(),
+        date: z.date(),
+        updatedAt: z.date(),
+        draft: z.boolean().default(false),
+        tags: z.array(z.string()).optional(),
+        hero: z.object({
+          image: property(z.string()).editor({ input: 'media' }),
+          alt: z.string(),
+          caption: z.string().optional()
+        })
+      }),
     })
   }
 })
