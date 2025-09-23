17a03dd32d6e58e99a27d3fc50f1057ab942066f
Emanuele Nardi <emanuelenardi.dev@gmail.com>
2025-09-23 14:40:16 +0200

    add header and footer content to app.vue

--- app/app.vue
+++ app/app.vue
@@ -5,7 +5,13 @@
   </div> -->
   <NuxtLayout>
     <UApp>
+      <header>
+        Header content
+      </header>
       <NuxtPage />
+      <footer>
+        Footer content
+      </footer>
     </UApp>
   </NuxtLayout>
 </template>
