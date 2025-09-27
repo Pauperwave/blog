1006d15d6cf16c86c7d7f81f857e374ee5bcd7e3
Emanuele Nardi <emanuelenardi.dev@gmail.com>
2025-09-28 01:42:31 +0200

    style: adjust indentation for consistency in number utility functions

--- shared/utils/number.ts
+++ shared/utils/number.ts
@@ -1,8 +1,12 @@
 export const randomInt = (min: number, max: number): number =>
-    Math.floor(Math.random() * (max - min + 1)) + min;
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+export function randomFrom<T>(array: T[]): T {
+  return array[Math.floor(Math.random() * array.length)]!
+}
 
 export const formatNumber = (num: number, decimals = 0): string =>
-    num.toLocaleString('it-IT', { minimumFractionDigits: decimals, maximumFractionDigits: decimals });
+  num.toLocaleString('it-IT', { minimumFractionDigits: decimals, maximumFractionDigits: decimals });
 
 export const percentage = (value: number, decimals = 0): string =>
-    `${(value * 100).toLocaleString('it-IT', { minimumFractionDigits: decimals, maximumFractionDigits: decimals })}%`;
+  `${(value * 100).toLocaleString('it-IT', { minimumFractionDigits: decimals, maximumFractionDigits: decimals })}%`;
