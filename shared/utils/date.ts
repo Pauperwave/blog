a1c721322f953da974625d9dec2fa543f099f0d1
Emanuele Nardi <emanuelenardi.dev@gmail.com>
2025-09-28 01:42:23 +0200

    feat: comment out Day.js imports and utility functions in date.ts

--- shared/utils/date.ts
+++ shared/utils/date.ts
@@ -1,42 +1,42 @@
 // utils/date.ts
 
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
-import updateLocale from 'dayjs/plugin/updateLocale';
-import isYesterday from 'dayjs/plugin/isYesterday'
-import isToday from 'dayjs/plugin/isToday';
-import isTomorrow from 'dayjs/plugin/isTomorrow'
+// import dayjs from 'dayjs';
+// import relativeTime from 'dayjs/plugin/relativeTime';
+// import updateLocale from 'dayjs/plugin/updateLocale';
+// import isYesterday from 'dayjs/plugin/isYesterday'
+// import isToday from 'dayjs/plugin/isToday';
+// import isTomorrow from 'dayjs/plugin/isTomorrow'
 
-import 'dayjs/locale/it';
-import 'dayjs/locale/de';
+// import 'dayjs/locale/it';
+// import 'dayjs/locale/de';
 
 // extend dayjs with the plugins
-dayjs.extend(relativeTime);
-dayjs.extend(updateLocale);
-dayjs.extend(isYesterday);
-dayjs.extend(isToday);
-dayjs.extend(isTomorrow);
+// dayjs.extend(relativeTime);
+// dayjs.extend(updateLocale);
+// dayjs.extend(isYesterday);
+// dayjs.extend(isToday);
+// dayjs.extend(isTomorrow);
 
-dayjs.updateLocale('it', {
-  relativeTime: {
-    future: "fra %s",
-    past: "%s fa",
-    s: 'alcuni secondi',
-    m: 'un minuto',
-    mm: '%d minuti',
-    h: 'un\'ora',
-    hh: '%d ore',
-    d: 'un giorno',
-    dd: '%d giorni',
-    M: 'un mese',
-    MM: '%d mesi',
-    y: 'un anno',
-    yy: '%d anni'
-  }
-});
+// dayjs.updateLocale('it', {
+//   relativeTime: {
+//     future: "fra %s",
+//     past: "%s fa",
+//     s: 'alcuni secondi',
+//     m: 'un minuto',
+//     mm: '%d minuti',
+//     h: 'un\'ora',
+//     hh: '%d ore',
+//     d: 'un giorno',
+//     dd: '%d giorni',
+//     M: 'un mese',
+//     MM: '%d mesi',
+//     y: 'un anno',
+//     yy: '%d anni'
+//   }
+// });
 
 // Checking the current Day.js locale
-dayjs.locale() // 'it' is now the default locale
+// dayjs.locale() // 'it' is now the default locale
 
 /**
  * Restituisce una data formattata come stringa secondo il formato specificato
@@ -48,51 +48,51 @@ dayjs.locale() // 'it' is now the default locale
  * @see https://day.js.org/docs/en/display/format
  * @see https://day.js.org/docs/en/display/format#list-of-all-available-formats
  */
-export const formatDate = (
-  date: Date | number | string,
-  format: string = 'DD/MM/YYYY'
-): string => {
-  return dayjs(date).format(format);
-};
+// export const formatDate = (
+//   date: Date | number | string,
+//   format: string = 'DD/MM/YYYY'
+// ): string => {
+//   return dayjs(date).format(format);
+// };
 
 /**
  * Restituisce una stringa "tempo relativo" in italiano o altra lingua
  */
-export const timeAgo = (date: Date | string | number, locale: string = 'it'): string => {
-  return dayjs(date).locale(locale).fromNow();
-};
+// export const timeAgo = (date: Date | string | number, locale: string = 'it'): string => {
+//   return dayjs(date).locale(locale).fromNow();
+// };
 
 // Controlla se la data è oggi
-export const isTodayDate = (date: Date | string | number): boolean => {
-  return dayjs(date).isToday();
-};
+// export const isTodayDate = (date: Date | string | number): boolean => {
+//   return dayjs(date).isToday();
+// };
 
 // Controlla se la data è ieri
-export const isYesterdayDate = (date: Date | string | number): boolean => {
-  return dayjs(date).isYesterday();
-};
+// export const isYesterdayDate = (date: Date | string | number): boolean => {
+//   return dayjs(date).isYesterday();
+// };
 
 // Controlla se la data è domani
-export const isTomorrowDate = (date: Date | string | number): boolean => {
-  return dayjs(date).isTomorrow();
-};
+// export const isTomorrowDate = (date: Date | string | number): boolean => {
+//   return dayjs(date).isTomorrow();
+// };
 
 // Controlla se la data è passata ignorando l'ora
-export const isPastDate = (date: Date | string | number): boolean => {
-  return dayjs(date).isBefore(dayjs(), 'day');
-};
+// export const isPastDate = (date: Date | string | number): boolean => {
+//   return dayjs(date).isBefore(dayjs(), 'day');
+// };
 
 // Controlla se la data è passata considerando anche l'ora
-export const isPastMoment = (date: Date | string | number): boolean => {
-  return dayjs(date).isBefore(dayjs());
-}
+// export const isPastMoment = (date: Date | string | number): boolean => {
+//   return dayjs(date).isBefore(dayjs());
+// }
 
 // Controlla se la data è futura ignorando l'ora
-export const isFutureDate = (date: Date | string | number): boolean => {
-  return dayjs(date).isAfter(dayjs(), 'day');
-};
+// export const isFutureDate = (date: Date | string | number): boolean => {
+//   return dayjs(date).isAfter(dayjs(), 'day');
+// };
 
 // Controlla se la data è futura considerando anche l'ora
-export const isFutureMoment = (date: Date | string | number): boolean => {
-  return dayjs(date).isAfter(dayjs());
-}
+// export const isFutureMoment = (date: Date | string | number): boolean => {
+//   return dayjs(date).isAfter(dayjs());
+// }
