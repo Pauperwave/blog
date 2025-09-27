6a2d7d6781ea63bc2fb3a86068159ed9a1f5280f
Emanuele Nardi <emanuelenardi.dev@gmail.com>
2025-09-27 23:57:35 +0200

    feat: update eslint configuration to enhance Vue component rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,27 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt({
-  rules: {
-    'vue/no-multiple-template-root': 'off',
-    'vue/max-attributes-per-line': ['error', { singleline: 3 }]
+export default withNuxt(
+  {
+    files: ['*.vue'],
+    rules: {
+      'flat/strongly-recommended': 'error',
+      'vue/multi-word-component-names': 'error',
+      'no-console': 'warn',
+      'no-debugger': 'error',
+    }
+  },
+  {
+    files: ['*.vue'],
+    rules: {
+      'vue/multi-word-component-names': 'error'
+    }
+  },
+  {
+    files: ['app.vue', 'error.vue', 'pages/**/*.vue', 'layouts/**/*.vue'],
+    rules: {
+      // disable the rule for these files
+      'vue/multi-word-component-names': 'off'
+    }
   }
-})
+)
