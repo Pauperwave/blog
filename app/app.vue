3bbe610937a7442850959420fdb1014741e2b5a8
Emanuele Nardi <emanuelenardi.dev@gmail.com>
2025-10-03 17:11:16 +0200

    refactor: comment out NuxtLayout and simplify template structure

--- app/app.vue
+++ app/app.vue
@@ -3,11 +3,14 @@
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div> -->
-  <NuxtLayout>
+  <!-- <NuxtLayout>
     <main class="container mx-auto p-4">
       <BlogHeader />
       <NuxtPage />
       <BlogFooter />
     </main>
-  </NuxtLayout>
+  </NuxtLayout> -->
+  <UApp>
+    <NuxtPage />
+  </UApp>
 </template>
