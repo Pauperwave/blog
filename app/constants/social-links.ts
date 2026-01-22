019a0ead388eb5d2e2d7b17625e9d24d25b20f70
Emanuele Nardi <emanuelenardi.dev@gmail.com>
2026-01-22 15:37:34 +0100

    refactor: update YouTube link to correct channel for accuracy

--- app/constants/social-links.ts
+++ app/constants/social-links.ts
@@ -4,7 +4,7 @@
 export const PAUPERWAVE_SOCIAL_LINKS = {
   facebook: 'https://www.facebook.com/pauperwave',
   instagram: 'https://www.instagram.com/pauperwave_official',
-  youtube: 'https://www.youtube.com/@pauperwave',
+  youtube: 'https://www.youtube.com/@alessandromoretti1177',
   telegram: 'https://t.me/pauperwave_official',
   github: 'https://github.com/pauperwave',
 } as const
