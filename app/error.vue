61a1ebec92042af5220430faffa2b9d668a97d86
Emanuele Nardi <emanuelenardi.dev@gmail.com>
2026-01-25 23:14:26 +0100

    fix: reorganize script and template structure in error.vue for improved readability

--- app/error.vue
+++ app/error.vue
@@ -1,9 +1,22 @@
+<script setup lang="ts">
+const error = useError();
+
+useSeoMeta({
+  title: error.value?.statusCode.toString(),
+  description: error.value?.statusMessage,
+});
+</script>
+
+
 <template>
   <UApp>
     <UHeader mode="slideover">
       <template #left>
-        <UUser name="Pauperwave Blog" to="/"
-          :avatar="{ src: 'https://avatars.githubusercontent.com/u/225214755?v=4' }" />
+        <UUser
+          name="Pauperwave Blog"
+          to="/"
+          :avatar="{ src: 'https://avatars.githubusercontent.com/u/225214755?v=4' }"
+        />
       </template>
     </UHeader>
     <UMain class="px-4">
@@ -17,12 +30,3 @@
     </UMain>
   </UApp>
 </template>
-
-<script setup lang="ts">
-const error = useError();
-
-useSeoMeta({
-  title: error.value?.statusCode.toString(),
-  description: error.value?.statusMessage,
-});
-</script>
