d47aaf2ea5348518897df81172da4bf0250d7dcb
Emanuele Nardi <emanuelenardi.dev@gmail.com>
2026-01-27 01:04:50 +0100

    feat: add new document page structure and remove obsolete pages

---
+++ app/pages/docs/[slug].vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+const route = useRoute()
+
+// Costruisci il path del documento basandoti sullo slug dell'URL
+const documentPath = computed(() => `docs/${route.params.slug}.md`)
+
+const { data } = await useAsyncData(`doc-${route.params.slug}`, () =>
+  queryCollection('docs').where('id', '=', documentPath.value).first()
+)
+
+const tocTitle = computed(() => `In questo articolo`);
+</script>
+
+<template>
+  <UPage :ui="{ center: 'lg:col-span-7!' }">
+    <template #right>
+      <UPageAside :ui="{ root: 'lg:col-span-3!' }">
+        <UPageAnchors
+          :links="[
+            { label: 'Canale YouTube', icon: 'mdi:youtube', to: 'https://www.youtube.com/@alessandromoretti1177', target: '_blank' },
+            { label: 'Tutti gli articoli', icon: 'material-symbols:article-rounded', to: '/articles' },
+          ]"
+        />
+        <USeparator
+          type="dotted"
+          class="mt-4"
+        />
+        <UContentToc
+          v-if="data"
+          :title="tocTitle"
+          :links="data.body.toc?.links"
+          highlight
+        />
+      </UPageAside>
+    </template>
+
+    <UPageHeader
+      :title="data?.title"
+      :description="data?.description"
+      headline="Documenti"
+    />
+
+    <UPageBody>
+      <ContentRenderer
+        v-if="data"
+        id="content"
+        :value="data"
+        class="markdown-content flex-1"
+      />
+    </UPageBody>
+  </UPage>
+</template>
+
+<style scoped>
+ol ol {
+  list-style-type: lower-alpha;
+}
+</style>
