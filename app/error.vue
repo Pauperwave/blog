1e8eb5b80971bb1e5253fc8e774f56c93c2d0754
Emanuele Nardi <emanuelenardi.dev@gmail.com>
2026-04-10 01:26:13 +0200

    style: format error.vue for improved readability and consistency

--- app/error.vue
+++ app/error.vue
@@ -15,25 +15,36 @@ useSeoMeta({
   <UApp>
     <UHeader mode="slideover">
       <template #left>
-        <UUser name="Pauperwave Blog" to="/"
-          :avatar="{ src: 'https://avatars.githubusercontent.com/u/225214755?v=4' }" />
+        <UUser
+          name="Pauperwave Blog"
+          to="/"
+          :avatar="{
+            src: 'https://avatars.githubusercontent.com/u/225214755?v=4'
+          }"
+        />
       </template>
     </UHeader>
     <UMain class="px-4">
       <UContainer>
-        <UError :clear="{
-          size: 'xl',
-          color: 'primary',
-          icon: 'i-lucide-arrow-left',
-          class: 'rounded-full',
-          label: 'Torna al sicuro'
-        }" :error="{
+        <UError
+          :clear="{
+            size: 'xl',
+            color: 'primary',
+            icon: 'i-lucide-arrow-left',
+            class: 'rounded-full',
+            label: 'Torna al sicuro'
+          }"
+          :error="{
             ...error,
             statusMessage: 'Ti senti perso?',
             message: 'Fblthp ha sempre odiato la folla.'
-          }">
-          <img src="~/assets/404/Fblthp-the-Lost-War-of-the-Spark-Art.jpg" alt="Fblthp the Lost"
-            class="mx-auto mb-6 rounded-xl" />
+          }"
+        >
+          <img
+            src="~/assets/404/Fblthp-the-Lost-War-of-the-Spark-Art.jpg"
+            alt="Fblthp the Lost"
+            class="mx-auto mb-6 rounded-xl"
+          />
         </UError>
       </UContainer>
     </UMain>
