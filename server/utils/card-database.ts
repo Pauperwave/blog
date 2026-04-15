ccde882866a36f89b159a9b058bd42442621bf68
Emanuele Nardi <emanuelenardi.dev@gmail.com>
2026-04-15 13:58:41 +0200

    fix: Import buildLog utility for enhanced database path debugging

--- server/utils/card-database.ts
+++ server/utils/card-database.ts
@@ -2,7 +2,7 @@
 import { join, dirname } from 'path'
 import { existsSync } from 'fs'
 import { fileURLToPath } from 'url'
-import { isServerlessEnvironment } from '#shared/utils'
+import { isServerlessEnvironment, buildLog } from '#shared/utils'
 
 export interface CardData {
   name: string
