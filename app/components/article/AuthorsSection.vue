741f8f24bba9f49ebd3baaf4bb922cf2db71b708
Emanuele Nardi <emanuelenardi.dev@gmail.com>
2026-04-17 00:12:20 +0200

    feat: Add AuthorsSection component to display author information in articles

---
+++ app/components/article/AuthorsSection.vue
@@ -0,0 +1,29 @@
+<!-- app\components\Article\AuthorsSection.vue -->
+<script setup lang="ts">
+import type { Author } from '~/composables/useAuthor'
+
+defineProps<{
+  authors: Author[] | undefined
+}>()
+</script>
+
+<template>
+  <template v-if="authors && authors.length > 0">
+    <div class="flex flex-col gap-4">
+      <AuthorAboutCard
+        v-for="author in authors"
+        :key="author.name"
+        :author="author"
+      >
+        <template
+          v-if="author.bio"
+          #body
+        >
+          <p class="text-sm text-muted mt-2">
+            {{ author.bio }}
+          </p>
+        </template>
+      </AuthorAboutCard>
+    </div>
+  </template>
+</template>
